7d0d687b68c96b361422e19ef7db6390ce90ceb1
IshantL <ishantlambhate@gmail.com>
2019-03-08 11:37:16 +0000

    Adding logic and redux connectivity

--- src/components/containers/search_bar.js
+++ src/components/containers/search_bar.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
-export default class SearchBar extends Component{
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {fetchWeather } from '../../actions/index';
+
+class SearchBar extends Component{
 	
 	constructor(props){
 		super(props);
 		this.state={term:''};
 		this.onInputChange=this.onInputChange.bind(this);
+		this.onFormSubmit=this.onFormSubmit.bind(this);
 	}
 	onInputChange(event){
 		this.setState({term:event.target.value});
+
 	}
 
 	onFormSubmit(event){
 		event.preventDefault();
+	     this.props.fetchWeather(this.state.term);
+		this.setState({term:''});
 	}
 	render(){
 		return(
@@ -27,3 +35,9 @@ export default class SearchBar extends Component{
 	);
 	}
 }
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({fetchWeather},dispatch);
+}
+
+export default connect(null,mapDispatchToProps)(SearchBar);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
-
+import WhetherReducer from './reducer_whether'
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //state: (state = {}) => state
+  weather:WhetherReducer
 });
 
 export default rootReducer;
